421d8a95834c8dcc9df558ae53fa96ec559247ae
Jay Nguyen <jay@Jays-iMac.home>
2021-11-22 20:54:20 -0800

    Created Seed Data for justDogs, Updated DogModel for Proper Exports, Update OwnerModel per Alyshia

--- models/OwnerModel.js
+++ models/OwnerModel.js
@@ -1,11 +1,17 @@
 const mongoose = require('../db/connection');
-const dogSchema = require('./DogModel')
-
+const dogSchema = require('./DogModel');
 const ownerSchema = new mongoose.Schema({
-	name : String,
-    city: String,
-    email: String,
-    dogs: [dogSchema]
+	name: String,
+	city: String,
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	dogs: [{ type: mongoose.Schema.ObjectId, ref: 'dogSchema' }],
 });
 
 module.exports = mongoose.model('DogOwner', ownerSchema);
