500ac09ca2868fcc659461aae6e4fc250b022f73
alyshia bentley <alyshiabentley@gmail.com>
2021-11-23 14:30:23 -0800

    wtf

--- models/OwnerModel.js
+++ models/OwnerModel.js
@@ -1,10 +1,11 @@
 const mongoose = require('../db/connection');
+const dogSchema = require('./DogModel')
 
 const ownerSchema = new mongoose.Schema({
 	name : String,
     city: String,
     email: String,
-    dogs: [{ ref: 'Dog', type: mongoose.Schema.Types.ObjectId }]
+    dogs: [dogSchema]
 });
 
 module.exports = mongoose.model('DogOwner', ownerSchema);
