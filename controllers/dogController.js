1e74201f3b3ecd4f2b0c4d1bac815f18a665475c
alyshia bentley <alyshiabentley@gmail.com>
2021-11-22 18:35:32 -0800

    update to async

--- controllers/dogController.js
+++ controllers/dogController.js
@@ -2,30 +2,31 @@ const express = require('express');
 const router = express.Router();
 const Dog = require('../models/DogModel');
 
-router.post('/', (req, res) => {
-	Dog.create(req.body)
-		.then((dog) => res.status(201).json({ status: 201, data: dog }))
-		.catch((e) => console.log(e));
+router.post('/', async (req, res) => {
+	const createDog = await Dog.create(req.body)
+	res.status(201).json({ status: 201, owners: createDog });
 });
 
 //get
-router.get('/', (req, res) => {
-	Dog.find().then((dog) => res.status(200).json({ status: 200, data: dog }));
+router.get('/', async (req, res) => {
+	const dogs = await Dog.find({});
+res.status(200).json({ status: 200, data: dogs });
 });
 
 //update
 
-router.put('/:id', (req, res) => {
-	Dog.findByIdAndUpdate(req.params.id, req.body, { new: true }).then((dog) =>
-		res.status(200).json({ status: 200, data: dog })
-	);
+router.put('/:id', async (req, res) => {
+	const updatedDog = await Dog.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+	});
+	res.status(200).json({ status: 200, data: updatedDog });
 });
 
 //delete
-router.delete('/:id', (req, res) => {
-	Dog.findByIdAndDelete(req.params.id).then(() =>
-		Dog.find().then((dog) => res.json({ data: dog }))
-	);
+router.delete('/:id', async (req, res) => {
+	const deleteDog = await Dog.findByIdAndDelete(req.params.id);
+	const dogs = Dog.find();
+	res.status(204).json({ dogs: dogs });
 });
 
 module.exports = router;
