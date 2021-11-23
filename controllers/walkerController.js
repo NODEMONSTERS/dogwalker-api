d24e9fd6e7c20e8244c0e44845f9a347ed753c39
Jay Nguyen <jay@Jays-iMac.local>
2021-11-23 11:22:20 -0800

    Added Seed Data for dogWalkers, Updated walkerController.js per Error Message

--- controllers/walkerController.js
+++ controllers/walkerController.js
@@ -1,30 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const Walker = require('../models/WalkerModel');
+const Walkers = require('../models/WalkerModel');
 
 // GET ROUTE - get all dog walkers
 router.get('/', async (req, res) => {
-  const allWalkers = await Walkers.find();
-  res.status(200).json({ status: 200, allWalkers: allWalkers });
+	const allWalkers = await Walkers.find();
+	res.status(200).json({ status: 200, allWalkers: allWalkers });
 });
 
 // POST ROUTE - add new dog walker
 router.post('/', async (req, res) => {
-  const newWalker = await Walker.create(req.body);
-  res.status(201).json({ status: 201, newWalker: newWalker });
+	const newWalker = await Walker.create(req.body);
+	res.status(201).json({ status: 201, newWalker: newWalker });
 });
 
 // DELETE ROUTE - delete dog walker by id
 router.delete('/:id', async (req, res) => {
-  await Walker.findByIdAndDelete(req.params.id);
-  const allWalkers = await Walkers.find();
-  res.status(200).json({ status: 200, allWalkers: allWalkers });  
+	await Walker.findByIdAndDelete(req.params.id);
+	const allWalkers = await Walkers.find();
+	res.status(200).json({ status: 200, allWalkers: allWalkers });
 });
 
 // UPDATE ROUTE - update dog walker
 router.put('/:id', async (req, res) => {
-  const updatedWalker = Walker.findByIdAndUpdate(req.body.id, req.body, {new: true});
-  res.status(200).json({ status: 200, data: updatedWalker });
+	const updatedWalker = Walker.findByIdAndUpdate(req.body.id, req.body, {
+		new: true,
+	});
+	res.status(200).json({ status: 200, data: updatedWalker });
 });
 
 module.exports = router;
