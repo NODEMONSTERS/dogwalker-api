121749640c893ee068b52a77225947df43e92e34
alyshia bentley <alyshiabentley@gmail.com>
2021-11-24 13:46:27 -0800

    update registration and add salt t othe encryption

--- controllers/ownerAuthenticationController.js
+++ controllers/ownerAuthenticationController.js
@@ -3,10 +3,8 @@ const router = express.Router();
 const User = require('../models/OwnerModel');
 
 const passport = require('passport');
-const passportLocal = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
-
 
 router.post('/login', (req, res, next) => {
 	passport.authenticate('local', (err, user, info) => {
@@ -22,27 +20,56 @@ router.post('/login', (req, res, next) => {
 	})(req, res, next);
 });
 
-router.post('/register', (req, res) => {
-	User.findOne({ username: req.body.email }, async (err, doc) => {
-		if (err) throw err;
-		if (doc) res.send('User Already Exists');
-		if (!doc) {
-			const hashedPassword = await bcrypt.hash(req.body.password, 10);
+// on registration
+router.post('/register', async (req, res) => {
+	const { name, email, password, city } = req.body;
 
-			const newUser = new User({
-				name: req.body.name,
-				username: req.body.email,
-				password: hashedPassword,
-			});
-			await newUser.save();
-			res.send('User Created');
-		}
-	});
+	// check toi make sure email and password are entered
+	if (!email || !password) {
+		return res.status(400).json({ msg: 'Please enter required fields' });
+	}
+
+	//search for existing users by email
+	const user = await User.findOne({email});
+
+	// if a user is found, return error message
+	if(user){
+		return res.status(400).json({ msg: 'User Already Exists. Please Login' });
+	}
+
+	// salt the password to add extra security. if something goes wrong, send error
+	const salt = await bcrypt.genSalt(10);
+	if (!salt) {
+		return res.status(400).json({ msg: "Something got salty"})
+	} 
+
+	// hash the password as well, so now sending the password out into the open world.
+	//if something goes wrong, send and error
+	const hash = await bcrypt.hash(password, salt)
+	if (!hash ) throw Error ("Something didn't get hashed correctly")
+
+	//create a new user
+	const newUser = new User ({
+		name,
+		email,
+		password: hash,
+		city
+	})
+
+	//save the user, and if it does not save, send error
+	const savedUser = await newUser.save();
+	if (!savedUser) throw Error ("User was not saved")
+
+	
+	res.status(200).json(
+		"user created"
+		)
+
 });
 router.get('/user', async (req, res) => {
 	const owners = await User.find({});
 	res.status(200).json({ status: 200, owners: owners });
-	res.send(req.user); 
+	res.send(req.user);
 });
 
 module.exports = router;
