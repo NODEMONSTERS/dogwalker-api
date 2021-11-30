b6cc05ec032f5865a9267558041144d90876fc61
alyshia bentley <alyshiabentley@gmail.com>
2021-11-30 09:04:56 -0800

    ?

--- models/WalkerModel.js
+++ models/WalkerModel.js
@@ -10,7 +10,11 @@ const walkerSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
-	picture: String,
+	picture: {
+		type: String,
+		default:
+			'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+	},
 	city: String,
 });
 
